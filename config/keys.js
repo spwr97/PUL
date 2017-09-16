fd7a4bf506c91255d920038896931058b7a9431d
ASRAP <ASRAP>
2017-09-16 11:05:18 +0530

    updated keys

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,14 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyC8twx77HQ80Cf9VoFtdkVsTl0dsf5OZok",
-  authDomain: "pulapp-80d33.firebaseapp.com",
-  databaseURL: "https://pulapp-80d33.firebaseio.com",
-  storageBucket: "pulapp-80d33.appspot.com",
-  messagingSenderId: "549128301559"
+  apiKey: "AIzaSyCAKK3e-vERaYQEojfl4V1lTyxEISrh_m0",
+  authDomain: "pulapp-f6a19.firebaseapp.com",
+  databaseURL: "https://pulapp-f6a19.firebaseio.com",
+  storageBucket: "pulapp-f6a19.appspot.com",
+  messagingSenderId: "674771561856"
 };
 
 export const lyftClientId = "vIW8hjXUoqPU";
 
-export const googleApiKey = "AIzaSyBisYcfer0ncVkLuHnb7XSdet4l8wBXR74";
+export const googleApiKey = "AIzaSyABrHGCWRH3-r3LCNCdoXvLot_M8wBuUNY";
 
 export const sentryURL =
   "https://a63f65fabe714c39bc843d0442a8b86b@sentry.io/124472";
